586ceffdec2a7ba002266967c332899e8a09ba6a
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2014-09-16 22:54:22 +0200

    handle exit msg when waiting call response

--- lib/exos.ex
+++ lib/exos.ex
@@ -18,7 +18,10 @@ defmodule Exos.Proc do
 
   def handle_call(term,_reply_to,port) do
     send(port,{self,{:command,:erlang.term_to_binary(term)}})
-    res = receive do {^port,{:data,b}}->:erlang.binary_to_term(b) after 1000-> :timeout end
+    res = receive do 
+      {^port,{:data,b}}->:erlang.binary_to_term(b)
+      {^port,{:exit_status,_}}=exit_msg->send(self,exit_msg);{:error,:port_terminated} # catch exit msg and resend it
+    end
     {:reply,res,port}
   end
 end
