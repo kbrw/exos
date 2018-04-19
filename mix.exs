cc94a80f1ceccc7d1293f3173e6361d4aeedca92
Jean Parpaillon <jean.parpaillon@free.fr>
2018-04-19 12:54:51 +0200

    Upgrade version to 1.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exos.Mixfile do
 
   def project do
     [app: :exos,
-     version: "1.0.1",
+     version: "1.0.3",
      elixir: ">= 1.0.0",
      description: description(),
      package: package(),
