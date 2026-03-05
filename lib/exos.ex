d5d91e932c0708dc9a8cee97d07941b6a74b4465
le_l <victor.le@kbrwadventure.com>
2026-03-05 12:56:44 +0100

    Replace Keyword.validate in order to be works with exos minimal elixir version

--- lib/exos.ex
+++ lib/exos.ex
@@ -38,21 +38,19 @@ defmodule Exos.Proc do
   end
 
   def start_link(cmd, init, opts \\ []) do
-    known_opts = [:port_opts, :gen_server_opts, :event_fun, :etf_opts]
-    case Keyword.keyword?(opts) and match?({:ok, _}, Keyword.validate(opts, known_opts)) do
-      true ->
-        port_opts = Keyword.get(opts, :port_opts, [])
-        gen_server_opts = Keyword.get(opts, :gen_server_opts, [])
-        event_fun = Keyword.get(opts, :event_fun)
-        etf_opts = Keyword.get(opts, :etf_opts, [])
-        GenServer.start_link(Exos.Proc, {cmd, init, port_opts, event_fun, etf_opts}, gen_server_opts)
-
-      false ->
-        # If no key matches with our opts, then it means it is the old API being used.
-        IO.warn("You are using the old API of #{__MODULE__}.start_link/3 that takes Port.open/2
-          options as 3rd parameter. The new API takes a Keyword list instead to handle options of
-          the old API.")
-        start_link(cmd, init, [port_opts: opts])
+    supported_opts = [:port_opts, :gen_server_opts, :event_fun, :etf_opts]
+    if Keyword.keyword?(opts) and Enum.all?(Keyword.keys(opts), & &1 in supported_opts) do
+      port_opts = Keyword.get(opts, :port_opts, [])
+      gen_server_opts = Keyword.get(opts, :gen_server_opts, [])
+      event_fun = Keyword.get(opts, :event_fun)
+      etf_opts = Keyword.get(opts, :etf_opts, [])
+      GenServer.start_link(Exos.Proc, {cmd, init, port_opts, event_fun, etf_opts}, gen_server_opts)
+    else
+      # If no key matches with our opts, then it means it is the old API being used.
+      IO.warn("You are using the old API of #{__MODULE__}.start_link/3 that takes Port.open/2
+        options as 3rd parameter. The new API takes a Keyword list instead to handle options of
+        the old API.")
+      start_link(cmd, init, [port_opts: opts])
     end
   end
 
